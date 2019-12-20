b670cbc194db551775aeb14fb8a93b2e5da7681d
Dhaval Pantojee <dpantojee775@gmail.com>
2019-12-20 14:57:03 +0530

    Revert "added static files and template dirs location according to the frontend app."
    This reverts commit b4bfba2f9db92bc45c228289b978226b9e8735c7.

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -121,11 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, os.path.join('frontend', 'static'))
-]
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, os.path.join('frontend', 'templates')),
-)
