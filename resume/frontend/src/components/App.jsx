5a7e952686b8660c866274bb327ed258fa4cd040
Dhaval Pantojee <dpantojee775@gmail.com>
2019-12-20 14:38:02 +0530

    changed app.js to app.jsx because .jsx files are easier to model in.

---
+++ resume/frontend/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <span>Hello World!</span>
+
+        <span>This really works!</span>
+      </div>
+    );
+  }
+}
