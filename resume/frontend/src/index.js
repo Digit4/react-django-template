05537a2d1a5fcffbdb183e60e4b88ad57c759010
Dhaval Pantojee <dpantojee775@gmail.com>
2019-12-20 13:53:49 +0530

    removed service worker because it was causing issues and it was unnecessary anyways.

--- resume/frontend/src/index.js
+++ resume/frontend/src/index.js
@@ -1,7 +1,7 @@
 import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 //registerServiceWorker();
